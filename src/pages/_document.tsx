4ffeb335d2903779d67d335d467d586f1df342cb
sihyonn <sihyeon982@naver.com>
2024-03-31 10:24:57 +0900

    feat: styled-components 설정 추가 및 테스트

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import Link from 'next/link';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  return() {
     <Html lang="ko">
       <Head>
         <Link
@@ -22,6 +48,6 @@ export default function Document() {
         <NextScript />
         <div id="modal-root"></div>
       </body>
-    </Html>
-  );
+    </Html>;
+  }
 }

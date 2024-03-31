70641cf3519409dabd58353733e491a5e7813916
sihyonn <sihyeon982@naver.com>
2024-03-31 23:31:16 +0900

    chore: 로그인 폼 정보 상수로 임시 분리

---
+++ src/constants/FORM_INFO.ts
@@ -0,0 +1,28 @@
+export const SIGN_IN = Object.freeze([
+  {
+    id: 'email',
+    type: 'text',
+    placeholder: '이메일을 입력해 주세요.',
+    validation: {
+      required: '이메일을 입력해 주세요.',
+      pattern: {
+        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        message: '올바른 이메일 주소가 아닙니다',
+      },
+    },
+  },
+  {
+    id: 'password',
+    type: 'password',
+    placeholder: '비밀번호를 입력해 주세요.',
+    validation: {
+      required: '비밀번호를 입력해 주세요.',
+      pattern: {
+        value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=\-{}[\]|\\:;"'<>,.?/~`])[\S]{8,20}$/,
+        message: '영문, 숫자, 특수문자를 포함해 8~20글자 입니다!',
+      },
+      minLength: { value: 8, message: '비밀번호는 8글자 이상입니다' },
+      maxLength: { value: 20, message: '비밀번호는 20글자 이내입니다' },
+    },
+  },
+]);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,37 +4,7 @@ import MainLayout from '@/components/template/MainLayout';
 import routes from '@/constants/routes';
 import Form from '@/components/common/form/Form';
 import Layout from '@/components/template/Layout';
-
-const mock = [
-  {
-    id: 'eamil',
-    label: '이메일',
-    type: 'text',
-    placeholder: '닉네임 입력(특수문자 제외, 6글자 이내)',
-    validation: {
-      required: '닉네임을 입력해주세요',
-      pattern: {
-        value: /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/,
-        message: '특수문자를 제외하고 입력해주세요',
-      },
-      maxLength: { value: 6, message: '6글자 이내로 입력해주세요!' },
-    },
-  },
-  {
-    id: 'password',
-    label: '비밀번호',
-    type: 'password',
-    placeholder: '비밀번호 입력',
-    validation: {
-      required: '비밀번호를 입력해주세요',
-      pattern: {
-        value: /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/,
-        message: '특수문자를 제외하고 입력해주세요',
-      },
-      maxLength: { value: 12, message: '12글자 이내로 입력해주세요!' },
-    },
-  },
-];
+import { SIGN_IN } from '@/constants/FORM_INFO';
 
 export default function Home() {
   const router = useRouter();
@@ -60,7 +30,7 @@ export default function Home() {
             onSubmit={() => {
               console.log('제출');
             }}
-            inputInfo={mock}
+            inputInfo={SIGN_IN}
             defaultValues={{ nickname: '' }}
           />
         </div>

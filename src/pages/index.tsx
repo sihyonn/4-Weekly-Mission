10c0c370a0d8e0318f5e1cc400a4eebd6ddf37c7
sihyonn <sihyeon982@naver.com>
2024-03-31 18:29:10 +0900

    chore: 기존 pages 폴더 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-  width: 300px;
-  background-color: yellow;
-`;
+import { useRouter } from 'next/router';
+import Button from '@/components/common/button/Button';
+import MainLayout from '@/components/template/MainLayout';
+import routes from '@/constants/routes';
 
 export default function Home() {
-  return <Container>안녕sdf</Container>;
+  const router = useRouter();
+
+  const goToSharedPage = () => {
+    router.push(routes.shared);
+  };
+
+  const goToFoldersPage = () => {
+    router.push(routes.folder);
+  };
+
+  const goToTest = () => {
+    router.push('/test');
+  };
+
+  return (
+    <MainLayout>
+      <div style={{ height: '100vh' }}>
+        <div>MainPage comming soon</div>
+        <Button style={{ margin: '2rem 1rem 0 0 ' }} onClick={goToSharedPage}>
+          공유페이지 바로가기
+        </Button>
+        <Button onClick={goToFoldersPage}>폴더페이지 바로가기</Button>
+        <Button onClick={goToTest}>테스트 바로가기</Button>
+      </div>
+    </MainLayout>
+  );
 }

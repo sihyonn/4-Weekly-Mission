62ee27cec18c0554648110e2a8301d20e481931a
sihyonn <sihyeon982@naver.com>
2024-03-31 10:48:26 +0900

    chore: 기존 api, hooks 폴더 추가

---
+++ src/hooks/api/user/useUserInfoQuery.ts
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+
+import userAPI from '@/api/userAPI';
+import API from '@/constants/API';
+
+interface UserQueryParams {
+  userId: number;
+}
+
+function useUserInfoQuery({ userId }: UserQueryParams) {
+  return useQuery({
+    queryKey: [API.USER.DETAIL(userId)],
+    queryFn: async () => {
+      return await userAPI.getUserInfo(userId);
+    },
+  });
+}
+
+export default useUserInfoQuery;

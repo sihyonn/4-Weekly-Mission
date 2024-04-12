f4ea41c041c185c69f087a489981e5075fdd06c6
sihyonn <sihyeon982@naver.com>
2024-04-12 23:55:53 +0900

    fix: authAPI async await 제거

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -5,8 +5,8 @@ import { LoginForm } from '@/interfaces/componentsInterface';
 /**
  * 이메일 중복 확인 api
  */
-const isEmailDuplicate = async (email: string) => {
-  return await instance({
+const isEmailDuplicate = (email: string) => {
+  return instance({
     url: API.AUTH.EMAIL_DUPLICATE,
     method: 'POST',
     data: { email },
@@ -16,8 +16,8 @@ const isEmailDuplicate = async (email: string) => {
 /**
  * 회원가입 요청 api
  */
-const postSignUp = async ({ email, password }: LoginForm) => {
-  return await instance({
+const postSignUp = ({ email, password }: LoginForm) => {
+  return instance({
     url: API.AUTH.SIGN_UP,
     method: 'POST',
     data: { email, password },
@@ -27,8 +27,8 @@ const postSignUp = async ({ email, password }: LoginForm) => {
 /**
  * 로그인 요청 api
  */
-const postSignIn = async ({ email, password }: LoginForm) => {
-  return await instance({
+const postSignIn = ({ email, password }: LoginForm) => {
+  return instance({
     url: API.AUTH.SIGN_IN,
     method: 'POST',
     data: { email, password },
